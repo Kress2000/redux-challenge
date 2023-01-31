1a5936b559a5824e8ae2ae1a3b43c51c4489d488
nsanzimfura eric <erickykress1@gmail.com>
2023-02-01 00:00:03 +0200

    persist

--- src/index.js
+++ src/index.js
@@ -5,18 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import TodoReducer from './Components/Todos'
+import TodoReducer from './Components/Todos';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+// import { combineReducers } from '@reduxjs/toolkit';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const persistConfig = {
+  key: "todos",
+  storage
+}
+const persistedReducer = persistReducer(persistConfig, TodoReducer);
 const store = configureStore({
-  reducer: {
-    todos: TodoReducer 
-  }
-})
+  reducer: {todos: persistedReducer }
+}) 
+const persistor = persistStore(store);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
